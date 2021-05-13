0580e68399f92faf56dd90cac45b0d223c8cbd3a
KyoungHwan Kim <kimkh7534@naver.com>
2021-05-13 15:59:21 +0900

    add search method

--- adsearch/src/containers/SearchContainer.js
+++ adsearch/src/containers/SearchContainer.js
@@ -1,24 +1,45 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Input } from 'antd';
 
 import SearchCard from '../components/SearchCard';
-import CardData from '../datas/CardData.json'
+import CardData from '../datas/CardData.json';
 
 const { Search } = Input;
 
 
 const SearchContainer = () => {
+    const Data = CardData.body.items;
+    const [Query, setQuery] = useState('');
+
+    const [Items, setItems] = useState();
+
+    const handleQuery = (e) => {
+        setQuery(e.target.value);
+    };
+
+    const handleSearchButton = () => {
+        console.log(Query);
+        let items = [];
+        for (var i = 0; i < Data.length; i++) {
+            if (Data[i].PRDUCT.indexOf(Query) != -1) {
+                items.push(Data[i]);
+            }
+        }
+        setItems(items);
+    };
+
     return (
         <Fragment>
             <div>
                 <Search
                     placeholder="검색"
-                    onSearch={(value) => console.log(value)}
-                    style={{ width: 300 }} 
+                    onSearch={handleSearchButton}
+                    onChange={handleQuery}
+                    style={{ width: 300 }}
                 />
             </div>
             <div>
-                {CardData.body.items.map((data) => {
+                {Items && Items.map((data) => {
                     return <SearchCard data={data} />
                 })}
             </div>
